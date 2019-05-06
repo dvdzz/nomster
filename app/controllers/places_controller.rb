b1a3bcc2348ed2dc610f606219fabe7133a32a16
dvdzz <dvdzz76@icloud.com>
2019-05-06 22:31:47 +0000

    updated popper_js and bootstrap gems

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.order("name").page(params[:page]).per_page(5)
+    @places = Place.order("name").page(params[:page]).per_page(5) 
+    
+end
+
+  def new
+    @places = Place.all
   end
+
 end
