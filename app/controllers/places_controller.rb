11bc52faa415c938864a003a70850a2dbdab1676
dvdzz <dvdzz76@icloud.com>
2019-05-07 15:42:09 +0000

    broken pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,11 @@
 class PlacesController < ApplicationController
-
   def index
-    @places = Place.order("name").page(params[:page]).per_page(5) 
+    @places = Place.order(:id).paginate(:page => params[:page], :per_page => 5) 
     
-end
-
-  def new
-    @places = Place.all
   end
 
+  
+  def new
+    @place = Place.new
+  end
 end
