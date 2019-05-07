2fd20757c1e9c7d61fbb5ce246b42014ecf957f3
dvdzz <dvdzz76@icloud.com>
2019-05-07 16:29:55 +0000

    Josh helped me with pagination and my conrollers, fixed the issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'place#index'
-  resources :place
+  root 'places#index'
+  resources :places
 end
