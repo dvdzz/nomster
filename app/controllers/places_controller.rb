44f9902041da85c8da3fb3c9b3f06cb4352b0bd2
dvdzz <dvdzz76@icloud.com>
2019-05-10 00:43:43 +0000

    Issue when resolving 'name' to newly created 'show'page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,7 +8,6 @@ class PlacesController < ApplicationController
     
   end
 
-  
   def new
     @place = Place.new
   end
@@ -17,6 +16,11 @@ class PlacesController < ApplicationController
     current_user.places.create(place_params)
     redirect_to root_path
   end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
 
   private
 
