32e32d779f964995ad256bd1689dbf27210f4db6
dvdzz <dvdzz76@icloud.com>
2019-05-11 16:48:36 +0000

    after completing lesson 26 and testing receive error no method error in placesshow

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all
@@ -12,8 +12,19 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
   end
 
   def show
@@ -30,12 +41,18 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -47,12 +64,3 @@ class PlacesController < ApplicationController
     @place.destroy
     redirect_to root_path
   end
-
-
-  private
-
-  def place_params
-    params.require(:place).permit(:name, :description, :address)
-  end
-
-end
