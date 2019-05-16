2dc95931c59b50f5b922eb1cf9aebfb1b680cce8
dvdzz <dvdzz76@icloud.com>
2019-05-16 23:39:18 +0000

    adding photos option, broken app.  HELP!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,6 @@ class CommentsController < ApplicationController
     private
 
   def comment_params
-    params.require(:comment).permit(:message, :rating)
+    params.require(:photo).permit(:caption)
   end
 end
