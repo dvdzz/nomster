a9ad36cc14d07568585cbb09a3292738e306ef13
dvdzz <dvdzz76@icloud.com>
2019-06-04 23:44:13 +0000

    app is broken on local:3030

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-    if Rails.env.production?
+    #if Rails.env.production?
         CarrierWave.configure do |config|
         config.root = Rails.root.join('tmp')
         config.cache_dir = 'carrierwave'
@@ -9,6 +9,6 @@
           :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],         # change var's name
           :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
         }
-        config.fog_directory     =  ENV['S3_BUCKET']
-      end
-    end
+        config.fog_directory      =  ENV['S3_BUCKET']
+        end
+    #end
