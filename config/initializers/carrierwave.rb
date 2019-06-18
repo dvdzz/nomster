a7d07f896ba5789b80e1dc189314ba98da0fd220
dvdzz <dvdzz76@icloud.com>
2019-06-18 17:44:25 +0000

    app broken locally

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    :provider           =>   'AWS',                        # required
+    :aws_access_key_id  =>   ENV["AWS_ACCESS_KEY"],        # required
+    :aws_secret_access_key =>ENV["AWS_SECRET_KEY"],
+    #:region                => 'us-east-1'               # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]
+  #config.fog_region    = ENV["AWS_REGION"]              # required
 end
