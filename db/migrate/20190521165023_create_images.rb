719bd9154cd70cbe825077eb00b8da9050a1246e
dvdzz <dvdzz76@icloud.com>
2019-07-02 19:36:17 +0000

    still tying to figure out photos issue.

--- db/migrate/20190521165023_create_images.rb
+++ db/migrate/20190521165023_create_images.rb
@@ -1,6 +1,6 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
-      create_table :images do |t|
+    create_table :images do |t|
       t.text :caption
       t.integer :user_id
       t.integer :place_id
