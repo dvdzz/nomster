9cf66a1b5ca3ec28a98bfcc2d04dcd4e007b54db
dvdzz <dvdzz76@icloud.com>
2019-07-16 20:58:11 +0000

    still unable to load pictures and error on profile page

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :images
+  has_many   :comments
+  has_many   :images
   
 
   validates :name, length: { minimum: 3, too_short: " - %{count} characters is the minimum allowed" }
