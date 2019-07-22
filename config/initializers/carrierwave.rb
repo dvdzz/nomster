d83e7da157f63ad4f4045008de25d4d2c1ac0da4
dvdzz <dvdzz76@icloud.com>
2019-07-22 22:05:07 +0000

    removed 'from AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@
 # config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
   config.fog_credentials = {
-  :provider => ‘AWS’,
+  :provider => AWS,
   :aws_access_key_id => ENV[“AWS_ACCESS_KEY”],
   :aws_secret_access_key => ENV[“AWS_SECRET_KEY”],
   :region => ENV[‘S3_REGION’]
